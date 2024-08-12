1c6572a1ad2b2032bdefa3573656d4abd683e3e0
Akash-more1 <akashmore9725@gmail.com>
2024-08-12 20:41:16 +0530

    ff

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,62 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useEffect, useState } from 'react';
+import '../styles/App.css';
 
+function App() {
+  const [markdown, setMarkdown] = useState('# Hello, Markdown');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
+  const parseMarkdown = (text) => {
+    let html = text;
+
+    // Headers
+    html = html.replace(/###### (.*$)/gim, '<h6>$1</h6>');
+    html = html.replace(/##### (.*$)/gim, '<h5>$1</h5>');
+    html = html.replace(/#### (.*$)/gim, '<h4>$1</h4>');
+    html = html.replace(/### (.*$)/gim, '<h3>$1</h3>');
+    html = html.replace(/## (.*$)/gim, '<h2>$1</h2>');
+    html = html.replace(/# (.*$)/gim, '<h1>$1</h1>');
+
+    // Bold
+    html = html.replace(/\*\*(.*)\*\*/gim, '<b>$1</b>');
+
+    // Italic
+    html = html.replace(/\*(.*)\*/gim, '<i>$1</i>');
+
+    // Line breaks
+    html = html.replace(/\n/gim, '<br>');
+
+    return html.trim();
+  };
+
+  return (
+    <div className='app'>
+      {loading ? (
+        <div className='loading'>Loading...</div>
+      ) : (
+        <>
+          <textarea
+            className='textarea'
+            value={markdown}
+            onChange={handleChange}
+          />
+          <div
+            className='preview'
+            dangerouslySetInnerHTML={{ __html: parseMarkdown(markdown) }}
+          ></div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
